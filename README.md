c69bbc80594a806b7e23f60c6134d42d84cbcee0
Daniel Graham <dan655t@gmail.com>
2022-01-09 11:34:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Danny's dotfiles
+
+This repo exists to simplify my process of configuring a new computer.
+It is based on [rcm](https://github.com/thoughtbot/rcm).
+
+## Setup
+
+Run `run.sh`.
---
+++ run.sh
@@ -0,0 +1,13 @@
+if ! command -v rcup &> /dev/null
+then
+    echo "rcup could not be found"
+    echo "please run the following:"
+    echo ""
+    echo "brew install rcm"
+    exit
+fi
+
+ln -fs "$HOME/dotfiles/.vscode/settings.json" "$HOME/Library/Application Support/Code/User"
+
+source rcrc
+rcup
